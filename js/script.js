216ef5ee9127e019eb3ab45170922f2bb5c79c37
Kamal KHATIM <kmlkhatim@gmail.com>
2025-04-13 15:09:17 +0100

    cree pour refaire

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// JS de menu responsive
-const menuIcon = document.querySelector('.bx-menu');
-const nav = document.querySelector('nav');
-
-menuIcon.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-});
